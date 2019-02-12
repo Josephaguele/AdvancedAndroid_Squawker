1c059e928a96a88264703e4eed54834e408ff301
Joseph <osekuemen@gmail.com>
2019-02-12 07:37:06 +0100

    Initial commit

---
+++ app/src/main/java/android/example/com/squawker/fcm/MyFirebaseInstanceIdService.java
@@ -0,0 +1,35 @@
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String TAG = MyFirebaseInstanceIdService.class.getSimpleName();
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+
+
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.i(TAG, "Refreshed token: " + refreshedToken);// Get new Instance ID token
+
+// If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+    }
+
+
+}
